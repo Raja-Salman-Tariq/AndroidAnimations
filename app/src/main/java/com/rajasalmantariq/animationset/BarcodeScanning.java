02e3014989f5df59ad2cffb68c3138c8def9ea97
Huzaifa-20 <huzaifa15192@gmail.com>
2020-12-29 02:55:37 +0500

    Text Animation also done

--- app/src/main/java/com/rajasalmantariq/animationset/BarcodeScanning.java
+++ app/src/main/java/com/rajasalmantariq/animationset/BarcodeScanning.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BarcodeScanning extends AppCompatActivity {
 
-    Animation bookFadeIn,bookZoom,phoneMovement,barcodeScanning,popUpMovement;
+    Animation bookFadeIn,bookZoom,phoneMovement,barcodeScanning,popUpMovement,instructionFade_1,instructionFade_2,instructionFade_3;
 
     ImageView book,phone,scanningBox,popUp;
+
+    TextView instructions_1,instructions_2,instructions_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,9 @@ public class BarcodeScanning extends AppCompatActivity {
         phoneMovement= AnimationUtils.loadAnimation(this, R.anim.phone_movement);
         barcodeScanning= AnimationUtils.loadAnimation(this, R.anim.barcode_scanning);
         popUpMovement= AnimationUtils.loadAnimation(this, R.anim.pop_up_movement);
+        instructionFade_1= AnimationUtils.loadAnimation(this, R.anim.instruction_fade);
+        instructionFade_2= AnimationUtils.loadAnimation(this, R.anim.instruction_fade_2);
+        instructionFade_3= AnimationUtils.loadAnimation(this, R.anim.instruction_fade_3);
     }
 
     private void setAnimatables() {
@@ -44,6 +50,11 @@ public class BarcodeScanning extends AppCompatActivity {
         phone= findViewById(R.id.cellIcon_barcode_scanning);
         scanningBox= findViewById(R.id.scanning);
         popUp= findViewById(R.id.pop_up);
+        instructions_1= findViewById(R.id.instructions_1);
+        instructions_2= findViewById(R.id.instructions_2);
+        instructions_2.setVisibility(View.INVISIBLE);
+        instructions_3= findViewById(R.id.instructions_3);
+        instructions_3.setVisibility(View.INVISIBLE);
     }
 
     private void setAnimToAnimatables() {
@@ -51,6 +62,9 @@ public class BarcodeScanning extends AppCompatActivity {
         phone.setAnimation(phoneMovement);
         scanningBox.setAnimation(barcodeScanning);
         popUp.setAnimation(popUpMovement);
+        instructions_1.setAnimation(instructionFade_1);
+        instructions_2.setAnimation(instructionFade_2);
+        instructions_3.setAnimation(instructionFade_3);
     }
 
     private void setAnimationListeners() {
@@ -62,12 +76,7 @@ public class BarcodeScanning extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-//                book.setScaleX((float) 0.5);
-//                book.setScaleY((float) 0.5);
-//                book.setPivotX(0);
-//                book.setPivotY(0);
                 book.setAnimation(bookZoom);
-
             }
 
             @Override
@@ -136,8 +145,57 @@ public class BarcodeScanning extends AppCompatActivity {
             public void onAnimationEnd(Animation animation) {
                 book.setScaleX((float) 0);
                 book.setScaleY((float) 0);
-//                book.setPivotX(0);
-//                book.setPivotY(0);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        instructionFade_1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                instructions_1.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        instructionFade_2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                instructions_2.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        instructionFade_3.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                instructions_3.setVisibility(View.INVISIBLE);
             }
 
             @Override
